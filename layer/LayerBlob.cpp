519d5495d4ce5603d8ed98644124b26d8b81c5ef
laoma02312 <974958656@qq.com>
2020-10-08 15:03:07 +0800

    blob

--- layer/LayerBlob.cpp
+++ layer/LayerBlob.cpp
@@ -4,16 +4,33 @@
 
 #include "LayerBlob.h"
 
-Blob::Blob(const int n, const int c, const int h, const int w, int type) {
+void Blob::_init(const int n, const int c, const int h, const int w, int type) {
     if ( type == TONES ){
         blobData = std::vector<arma::cube>(n, arma::cube(h, w, c, arma::fill::ones));
+        return;
     }
     if ( type == TZEROS){
         blobData = std::vector<arma::cube>(n, arma::cube(h, w, c, arma::fill::zeros));
+        return;
     }
     if ( type == TDEFAULT){
         blobData = std::vector<arma::cube>(n, arma::cube(h, w, c));
+        return;
     }
-    if ( type == TRANDU){}
-    if ( type == TRANDN){}
+    if ( type == TRANDU ) {
+        for (int i = 0; i < n; i++) {
+            blobData.emplace_back(arma::randu<arma::cube>(h, w, c));
+        }
+        return;
+    }
+    if ( type == TRANDN ){
+        for(int i = 0; i < n; i++){
+            blobData.emplace_back(arma::randn<arma::cube>(h,w,c));
+        }
+        return;
+    }
+}
+
+Blob::Blob(const int n, const int c, const int h, const int w, int type) {
+    _init(n ,c, h, w, type);
 }
